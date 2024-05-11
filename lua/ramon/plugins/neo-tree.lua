4cf3efb2df056a9ad705e648c9b38b18f5fcb2e2
Ramon de Araujo Borba <ramonborba97@gmail.com>
2024-05-11 19:18:13 -0300

    plugins: neotree: Update neotree config

--- lua/ramon/plugins/neo-tree.lua
+++ lua/ramon/plugins/neo-tree.lua
@@ -2,24 +2,28 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'MunifTanjim/nui.nvim',
     },
-  },
+    -- cmd = 'Neotree',
+    -- keys = {
+    --     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    -- },
+    config = function()
+        require('neo-tree').setup({
+            filesystem = {
+                hijack_netrw_behavior = 'open_default',
+                window = {
+                    mappings = {
+                        ['\\'] = 'close_window',
+                    },
+                },
+            },
+        })
+        vim.keymap.set('n', '\\', '<cmd>Neotree toggle<CR>', { desc = 'NeoTree toggle' })
+    end,
 }
