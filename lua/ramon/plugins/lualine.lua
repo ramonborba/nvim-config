c715786c3c999e0da70cef98b448d9b85ce6f439
Ramon de Araujo Borba <ramonborba97@gmail.com>
2024-06-02 22:49:27 -0300

    plugins: lualine: Update theme option and filename section

--- lua/ramon/plugins/lualine.lua
+++ lua/ramon/plugins/lualine.lua
@@ -4,7 +4,10 @@ return {
     config = function()
         require('lualine').setup({
             options = {
-                theme = 'auto',
+                theme = (vim.g.colors_name == 'arctic') and 'ayu_mirage' or 'auto',
+            },
+            sections = {
+                lualine_c = { { 'filename', path = 1 } },
             },
         })
     end,
